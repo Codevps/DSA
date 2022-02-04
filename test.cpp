97dec245ac5a7939e3465b6b04a349f986e034ab
Pratham Sawant <codevps07@gmail.com>
2022-02-04 14:52:52 +0530

    cpp

--- test.cpp
+++ test.cpp
@@ -440,3 +440,59 @@ void mergeSort(int *arr, int s, int e)
 
     merge(arr, s, e);
 }
+
+// quicksort
+int partition(int *arr, int s, int e)
+{
+    // swapping algorithm:
+    int pivot = arr[s];
+
+    int cnt = 0;
+    for (int i = s + 1; i < e; i++)
+    {
+        if (arr[i] <= pivot)
+        {
+            cnt++;
+        }
+    }
+    // pivot's right position
+    int pivotIndex = cnt + s;
+    // swapping them
+    swap(arr[pivotIndex], arr[s]);
+
+    // managing left and right part:
+    int i = s;
+    int j = e;
+    while (i < pivotIndex && j > pivotIndex)
+    {
+        while (arr[i] < pivot)
+        {
+            i++;
+        }
+        while (arr[j] > pivot)
+        {
+            j--;
+        }
+        if (i < pivotIndex && j > pivotIndex)
+        {
+            swap(arr[i++], arr[j--]);
+        }
+    }
+    return pivotIndex;
+}
+void quickSort(int *arr, int s, int e)
+{
+    // basecase:
+    if (s >= e)
+    {
+        return;
+    }
+    // partition
+    int p = partition(arr, s, e);
+
+    // left split
+    quickSort(arr, s, p - 1);
+
+    // right split
+    quickSort(arr, p + 1, e);
+}
